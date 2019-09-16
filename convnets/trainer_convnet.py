cee0cf5e495f08811f3ac470a767aaa053469ab3
Henry at Chappie <henryclever@gmail.com>
2019-09-16 13:03:59 -0400

    minor

--- convnets/trainer_convnet.py
+++ convnets/trainer_convnet.py
@@ -612,7 +612,7 @@ class PhysicalTrainer():
                     print_vals_list.append(train_loss)
 
                     print_text_list.append('\n\t\t\t\t\t  Val Total Loss: {:.2f}')
-                    print_vals_list.append(train_loss)
+                    print_vals_list.append(val_loss)
 
 
 
@@ -692,7 +692,7 @@ class PhysicalTrainer():
 
         for batch_i, batch in enumerate(self.test_loader):
 
-            print torch.cuda.max_memory_allocated(), "val", batch_i
+            #print torch.cuda.max_memory_allocated(), "val", batch_i
             scores, INPUT_DICT_VAL, OUTPUT_DICT_VAL = \
                 UnpackBatchLib().unpackage_batch_kin_pass(batch, is_training=False, model=self.model, CTRL_PNL=self.CTRL_PNL)
             scores_zeros = Variable(torch.Tensor(np.zeros((batch[0].shape[0], scores.size()[1]))).type(dtype),
